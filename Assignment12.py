d32d837bee1abf20b8daa661967837fb939617ad
Vivek Kumar <vivekgupta70509@gmail.com>
2025-04-09 04:01:45 +0530

    Add files via upload

---
+++ Assignment12.py
@@ -0,0 +1,34 @@
+#Q1
+x=0
+if x>0:
+    print("positive")
+else:
+    print("non positive")
+
+#Q2
+x=55
+if x%5==0:
+    print("divisible")
+else:
+    print("not divisible")
+
+
+#Q3
+x=1
+if x%2==0:
+    print("even")
+else:
+    print("odd")
+
+#Q4
+x=7
+y=7
+if x>y:
+    print(x)
+else:
+    print(y)
+
+#Q5
+x='y'
+y='z'
+print(x,y) if(x<y) else print(y,x)
